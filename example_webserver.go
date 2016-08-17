6bd6591019244a04e5567f41438e955c482057ff
Xavier Godart <xavier.godart64@gmail.com>
2016-08-17 18:44:44 +0200

    Add basic webserver for testing purpose

---
+++ example_webserver.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatalf("Usage: %s <port>", os.Args[0])
+	}
+	if _, err := strconv.Atoi(os.Args[1]); err != nil {
+		log.Fatalf("Invalid port: %s (%s)\n", os.Args[1], err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		println("--->", os.Args[1], req.URL.String())
+		//http.Error(w, "Erreur !", http.StatusInternalServerError)
+	})
+	http.ListenAndServe(":"+os.Args[1], nil)
+}
