40b02e22476adeaac4c1c0a50de90c09b46c2ee0
Xavier Godart <xavier.godart64@gmail.com>
2016-08-17 16:58:07 +0200

    Refactoring : move ReverseProxy code to middlewares package

--- server/server.go
+++ server/server.go
@@ -1,53 +1,23 @@
 package server
 
 import (
-	"log"
-	"net/url"
 	"net/http"
-	"github.com/vulcand/oxy/forward"
-	"github.com/vulcand/oxy/roundrobin"
-	"github.com/vulcand/oxy/stream"
+	"github.com/xaviergodart/gydro/middlewares"
 	"github.com/xaviergodart/gydro/models"
 )
 
 type Server struct {
-	EntryPoints map[string]*stream.Streamer
+	ReverseProxy *middlewares.ReverseProxy
 }
 
 func NewServer() *Server {
 	apis := models.FindAllApis()
-	conf := make(map[string]*stream.Streamer)
-
-	// Loading routing and backend configuration from database
-	for _, api := range apis {
-		fwd, _ := forward.New()
-		lb, _ := roundrobin.New(fwd)
-		for _, backend := range api.Backends {
-			target, _ := url.Parse(backend)
-			lb.UpsertServer(target)
-		}
-		stream, _ := stream.New(lb, stream.Retry(`(IsNetworkError() || ResponseCode() >= 500) && Attempts() < 2`))
-		conf[api.Route] = stream
-	}
-
-	return &Server{EntryPoints: conf}
+	reverseProxy := middlewares.NewReverseProxy(apis)
+	return &Server{ReverseProxy: reverseProxy}
 }
 
 func (s *Server) ListenAndServe(addr string) {
-	http.Handle("/", s)
+	http.Handle("/", s.ReverseProxy)
 	http.ListenAndServe(addr, nil)
 }
 
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("X-GydroProxy", "GydroProxy")
-	log.Println(r.URL.Path)
-	if backends, ok := s.EntryPoints[r.URL.Path]; ok {
-		log.Println("proxy: custom")
-		log.Println(ok)
-		backends.ServeHTTP(w, r)
-		return
-	}
-
-	log.Println("proxy: default")
-	http.NotFoundHandler().ServeHTTP(w, r)
-}
