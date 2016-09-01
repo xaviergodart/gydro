6de1e967d90ef22c6224e7d47ac6f437f0c18924
Xavier Godart <xavier.godart64@gmail.com>
2016-09-01 12:42:10 +0200

    Rename stream to buffer (oxy stream lib update) + move circuit breaker in the middleware chain

--- server/reverse_proxy.go
+++ server/reverse_proxy.go
@@ -3,28 +3,28 @@ package server
 import (
 	"github.com/vulcand/oxy/forward"
 	"github.com/vulcand/oxy/roundrobin"
-	"github.com/vulcand/oxy/stream"
+	"github.com/vulcand/oxy/buffer"
 	"github.com/vulcand/oxy/cbreaker"
 	"net/http"
 	"net/url"
 )
 
 type ReverseProxy struct {
-	stream *stream.Streamer
+	handler http.Handler
 }
 
 func NewReverseProxy(backends []string) *ReverseProxy {
 	fwd, _ := forward.New()
-	cb, _ := cbreaker.New(fwd, `NetworkErrorRatio() > 0.5`)
-	lb, _ := roundrobin.New(cb)
+	lb, _ := roundrobin.New(fwd)
 	for _, backend := range backends {
 		target, _ := url.Parse(backend)
 		lb.UpsertServer(target)
 	}
-	stream, _ := stream.New(lb, stream.Retry(`(IsNetworkError() || ResponseCode() >= 500) && Attempts() < 2`))
-	return &ReverseProxy{stream: stream}
+	buff, _ := buffer.New(lb, buffer.Retry(`(IsNetworkError() || ResponseCode() >= 500) && Attempts() < 2`))
+	cb, _ := cbreaker.New(buff, `NetworkErrorRatio() > 0.5`)
+	return &ReverseProxy{handler: cb}
 }
 
 func (rp *ReverseProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	rp.stream.ServeHTTP(w, r)
+	rp.handler.ServeHTTP(w, r)
 }
